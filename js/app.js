ba7cb87219cad83d14a0faa69657c4be327caf75
rachelpatrocinio <rachelannepatrocinio@gmail.com>
2024-03-19 15:09:27 +0100

    Refactor: cicle for

--- js/app.js
+++ js/app.js
@@ -13,15 +13,22 @@ const listOfVips = [
     'Maneskin'
 ]
 
+//SOLUZIONE CON CICLE for
+const tableName = "Tavolo VIP";
+let nameOfVip;
+let tablePlace = 0;
 
-// //SOLUZIONE CON IL CICLO FOR
-// for ( let i = 0; i < listOfVips.length; i++){
-//     const currentVip = listOfVips[i];
-//     currentVip.nameOfVip = currentVip;
-//     currentVip.tableName = 'Tavolo VIP';
-//     currentVip.tablePlace = i + 1;
-// }
-// console.log(listOfVips);
+for(let i = 0; i < listOfVips.length; i++){
+    const currentVip = listOfVips[i];
+    tablePlace++;
+
+    const object = {
+        nameOfVip: currentVip,
+        tableName: tableName,
+        tablePlace: tablePlace
+    }
+    console.log(object);
+}
 
 //SOLUZIONE CON method .map
 const object = listOfVips.map((el, i) => {
