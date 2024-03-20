946161be839ed32551e0236f199124dcbcae3456
rachelpatrocinio <rachelannepatrocinio@gmail.com>
2024-03-20 10:00:19 +0100

    Refactor: snack1 -

--- js/app.js
+++ js/app.js
@@ -13,29 +13,31 @@ const listOfVips = [
     'Maneskin'
 ]
 
-// //SOLUZIONE CON CICLE for
-// const tableName = "Tavolo VIP";
-// let nameOfVip;
-// let tablePlace = 0;
+//SOLUZIONE CON CICLE for
+const tableName = "Tavolo VIP";
+let nameOfVip;
+let tablePlace = 0;
 
-// for(let i = 0; i < listOfVips.length; i++){
-//     const currentVip = listOfVips[i];
-//     tablePlace++;
+const listOfVipsObject = [];
+for(let i = 0; i < listOfVips.length; i++){
+    const currentVip = listOfVips[i];
+    tablePlace++;
 
-//     const object = {
-//         nameOfVip: currentVip,
-//         tableName: tableName,
-//         tablePlace: tablePlace
-//     }
-//     console.log(object);
-// }
-
-//SOLUZIONE CON method .map
-const object = listOfVips.map((el, i) => {
-    return{
-        nameOfVip: el,
-        tableName: 'Tavolo VIP',
-        tablePlace: i+1
+    const object = {
+        nameOfVip: currentVip,
+        tableName: tableName,
+        tablePlace: tablePlace
     }
-})
-console.log(object);
+    listOfVipsObject.push(object);
+}
+console.log(listOfVipsObject);
+
+// //SOLUZIONE CON method .map
+// const object = listOfVips.map((el, i) => {
+//     return{
+//         nameOfVip: el,
+//         tableName: 'Tavolo VIP',
+//         tablePlace: i+1
+//     }
+// })
+// console.log(object);
